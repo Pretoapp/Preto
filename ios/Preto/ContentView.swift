140916248a4b9ca38b45bbf8769275e20a228f25
Preto <marinavillemo@gmail.com>
2024-08-05 20:24:48 -0300

    Ensure all necessary files are tracked and commit changes to .gitignore

---
+++ ios/Preto/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Preto
+//
+//  Created by Marina Lapsheva on 05/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios/Preto/PretoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PretoApp.swift
+//  Preto
+//
+//  Created by Marina Lapsheva on 05/08/24.
+//
+
+import SwiftUI
+
+@main
+struct PretoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
